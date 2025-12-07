98fc39a2875a5f1b1d18d038f7c44d24d7197e7f
ComfyUI Local <local@comfyui.local>
2025-12-07 13:22:37 +0800

    chore: Local customizations and fixes

--- src/custom_mesh_graphormer/modeling/bert/modeling_utils.py
+++ src/custom_mesh_graphormer/modeling/bert/modeling_utils.py
@@ -1,1 +1,7 @@
 from transformers.modeling_utils import *
+
+# These functions moved to pytorch_utils in newer transformers versions
+try:
+    from transformers.modeling_utils import prune_linear_layer, prune_layer, Conv1D
+except ImportError:
+    from transformers.pytorch_utils import prune_linear_layer, prune_layer, Conv1D
